e4453e79b1af342ea6d9889645c647f09e4bacbd
Vizente97 <vizente14@gmail.com>
2021-01-28 00:01:37 -0600

    Correxion codigos

--- Proyecto Embebidos/RGB.c
+++ Proyecto Embebidos/RGB.c
@@ -3,19 +3,19 @@
 #use delay(clock=2M)
 #use rs232(baud=9600,xmit=PIN_C6,rcv=PIN_C7, bits=8, parity=N)  
 
-int8 redLed;                                                            //Ciclo de trabajo para el led rojo (0-255)
-int8 greenLed;                                                          //Ciclo de trabajo para el led verde (0-255)
-int8 blueLed;                                                           //Ciclo de trabajo para el led azul (0-255)
+int8 redLed;                                                            //Ciclo para el led rojo (0-255)
+int8 greenLed;                                                          //Ciclo para el led verde (0-255)
+int8 blueLed;                                                           //Ciclo para el led azul (0-255)
 int8 counter;
 int8 i;
-char dato;                                                           //contador para comparar el ciclo de trabajo con el periodo del pwm
+char dato;                                                          
 
-#int_TIMER2                                                             //Interrupcion cada 50us para simular el pwm
+#int_TIMER2                                                           //Interrupcion para simular el pwm
 void  TIMER2_isr(void) 
 {  counter+=4; 
-   if(redLed>counter)                                                    //Se suman 4 para reducir la resolucion del pwm y evitar parpadeos en los
-  { output_high(PIN_B0);}                                             //leds ya que la frecuencia actual del microcontrolador no es suficiente
-  else                                                                   //para un pwm con resolucion de 255
+   if(redLed>counter)                                                    
+  { output_high(PIN_B0);}                                             
+  else                                                                   
   { output_low(PIN_B0);    }
   if(greenLed>counter)
   { output_high(PIN_B1);   }
@@ -27,7 +27,7 @@ void  TIMER2_isr(void)
   {  output_low(PIN_B2);   }
 }
 
-#INT_RDA                                                             //Interrupcion cada 50us para simular el pwm
+#INT_RDA                                                             //Interrupcion para obtener el dato de la interfaz.
 void  rda_isr(void) 
 {  
    dato = getc();
@@ -38,17 +38,17 @@ void main()
    //Configuramos los timers
    setup_timer_0(RTCC_INTERNAL|RTCC_DIV_1);
    setup_timer_1(T1_DISABLED);
-   setup_timer_2(T2_DIV_BY_1,49,1);                                     //Configuracion para interrupcion cada 50us
-   //setup_comparator(NC_NC_NC_NC);                                       //Deshabilitamos las entradas analógicas
+   setup_timer_2(T2_DIV_BY_1,49,1);                                     
+   //setup_comparator(NC_NC_NC_NC);                                     
    setup_vref(FALSE);
    SET_TRIS_B( 0b00000111 );                                            //Configuramos el puerto B 
    enable_interrupts(INT_TIMER2);                                       //Habilitamos las interrupciones del timer 2
    enable_interrupts(GLOBAL);
-   enable_interrupts(INT_RDA);
+   enable_interrupts(INT_RDA);                                          //Habilitamos las interrupciones del rda
    counter=0;
    redLed=0;
    greenLed=0;
-   blueLed=0;                                                   //Variable para el ciclo for
+   blueLed=0;                                                   
    while(true){
       counter=0;
       redLed=0;
@@ -78,12 +78,12 @@ void main()
             delay_ms(15);
          }
          redLed = 0;
-         for(i=0; i<254; i++)                                             //Variamos de negro a azul
+         for(i=0; i<254; i++)                                             //Variamos de negro a verde
          {  greenLed=i;
             delay_ms(15);
          }       
          greenLed = 0;
-         for(i=0; i<254; i++)                                            //Variamos negro a verde
+         for(i=0; i<254; i++)                                            //Variamos negro a azul
          {  blueLed=i;
             delay_ms(15);
          }
